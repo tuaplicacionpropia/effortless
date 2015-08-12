b0a7f8b9af694a106d337bf9bcb5d3a50690105e
tuaplicacionpropia <tuaplicacionpropia@gmail.com>
2015-08-12 22:43:26 +0100

    editor

---
+++ zkstrap/classes/org/effortless/zkstrap/Editor.java
@@ -0,0 +1,16 @@
+package org.effortless.zkstrap;
+
+public class Editor extends BaseEditor {
+
+	public Editor () {
+		super();
+//		org.zkoss.zk.ui.util.Template t = null;
+//		
+//		t.	
+//		div filter
+//		div top_buttons
+//		div list
+//		div bottom buttons
+	}
+	
+}
---
+++ zkstrap/web/js/org/effortless/zkstrap/Editor.js
@@ -0,0 +1,40 @@
+org.effortless.zkstrap.Editor = zk.$extends(org.effortless.zkstrap.BaseEditor, {
+
+  bind_ : function(evt) {
+    this.$supers('bind_', arguments);
+    
+//    this.appendChild(new org.effortless.zkstrap.Layout());    
+    
+  },
+
+  unbind_ : function(evt) {
+    this.$supers('unbind_', arguments);
+  }
+  
+/*
+  _layout: null, 
+
+  _addChild: function (child) {
+    if (this._layout != null) {
+      this._layout = new org.effortless.zkstrap.Layout();
+      this.$supers('appendChild', this._layout);
+    }
+	jq(this._layout.$n()).append(child.$n());
+  },
+
+  insertBefore: function (child, sibling, ignoreDom) {
+    this.$supers('insertBefore', arguments);
+    this._addChild(child);
+	return true;
+  },
+  
+  appendChild: function (child, ignoreDom) {
+    this.$supers('appendChild', arguments);
+    this._addChild(child);
+	return true;
+  }
+*/
+
+  
+  
+});
